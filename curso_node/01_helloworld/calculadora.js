512558cac8c97e0fb72666450e107590986718fa
Bryan Almeida <124791559+BryanAlmeida016@users.noreply.github.com>
2024-06-06 09:28:45 -0300

    Delete curso_node directory

--- curso_node/01_helloworld/calculadora.js
+++
@@ -1,9 +0,0 @@
-var somar = require("./modulos/somar")
-var subtrair = require("./modulos/subtrair")
-var multiplicar = require("./modulos/multiplicar")
-var dividir = require("./modulos/dividir")
-
-console.log(somar(10,5))
-console.log(subtrair(20,5))
-console.log(multiplicar(5,3))
-console.log(dividir(30,2))
